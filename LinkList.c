949c20fb4468f3392a3e82ee4203a7c3d7442e6d
Sarthak Gussain <itssarthgussain@gmail.com>
2026-08-13 00:43:35 +0530

    Add project files

---
+++ LinkList.c
@@ -0,0 +1,58 @@
+#include <stdio.h>
+#include <stdlib.h>
+int main(){
+    struct node{
+        int value;
+        struct node* nextnode;
+    }
+
+    struct node *head=NULL;
+
+    void push(int val){
+        struct node *newnode;
+        newnode=(struct node*)malloc(sizeof(struct node));
+        newnode->value=val;
+        newnode->nextnode=head;
+        head=newnode;
+
+    }
+
+    void pop()
+{
+    if (head == NULL) {
+        printf("Stack is empty\n");
+        return;
+    }
+
+    struct node *temp = head;
+    printf("Popped: %d\n", temp->value);
+
+    head = head->nextnode;
+
+    free(temp);
+
+}void display()
+{
+    struct node *temp = head;
+
+    while (temp != NULL) {
+        printf("%d ", temp->value);
+        temp = temp->nextnode;
+    }
+
+    printf("\n");
+}
+
+int main()
+{
+    push(10);
+    push(20);
+    push(30);
+
+    display();
+
+    pop();
+    display();
+
+    pop();
+    display();}
